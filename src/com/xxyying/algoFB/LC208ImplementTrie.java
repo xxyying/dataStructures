21854dc5ae843c9a4c5bd3f8f4b6dc13a77dc1e2
Yiying Liu <ceci.liuyiying@outlook.com>
2017-09-26 10:57:18 -0700

    -09/26

--- src/com/xxyying/algoFB/LC208ImplementTrie.java
+++ src/com/xxyying/algoFB/LC208ImplementTrie.java
@@ -1,6 +1,6 @@
 package com.xxyying.algoFB;
 
-public class LC208ImplementTree {
+public class LC208ImplementTrie {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
