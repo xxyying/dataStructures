925afe2cc226e4bca5614453fe766f77f1937124
Yiying Liu <ceci.liuyiying@outlook.com>
2017-09-29 15:59:51 -0700

    -09/29

---
+++ src/com/xxyying/algoFB/LC273IntegerToEnglishWords.java
@@ -0,0 +1,16 @@
+package com.xxyying.algoFB;
+
+public class LC273IntegerToEnglishWords {
+	public String numberToWords(int num) {
+        
+    }
+
+
+	public static void main(String[] args) {
+		LC273IntegerToEnglishWords test = new LC273IntegerToEnglishWords();
+		int num = 123;
+		String res = test.numberToWords(num);
+		System.out.println(res);
+	}
+
+}
